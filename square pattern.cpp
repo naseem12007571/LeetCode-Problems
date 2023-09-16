594be9564c273fb6228c4bfde4b0e7e8e252a2b5
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-09-16 19:46:31 +0530

    Create square pattern.cpp

---
+++ square pattern.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+  int row,col;
+  cin>>row>>col;
+  for(int i=0;i<row;i++){
+    for(int j=0j<col;j++){
+      cout<<"*"
+    }
+    cout<<endl;
+  }
+}
